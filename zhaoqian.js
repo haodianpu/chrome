1ae4f5bf3017fc10eb8a47604c89ef59fabf4467
周翔 <zhouxiang@haodianpu.com>
2015-08-12 16:03:48 +0800

    Update zhaoqian.js

--- zhaoqian.js
+++ zhaoqian.js
@@ -239,6 +239,7 @@ var ZHAO = (function(){
 			});
         },
         tradeDetail: function(search) {
+        		console.log(123456);
 			//给扩展发起请求，我要知道用户uid，只有知道了用户id才能干事
 			chrome.extension.sendRequest({uid: "please"}, function(response) {
 				console.log(response.uid);
